145eb16893689bfea315134488b12d733da382c4
samAricha <samaricha001.com>
2023-05-11 15:08:26 +0300

    first commit

---
+++ src/main/java/tum/devs/kmhfldemo/KmhflDemoApplication.java
@@ -0,0 +1,13 @@
+package tum.devs.kmhfldemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KmhflDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KmhflDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tum/devs/kmhfldemo/KmhflDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package tum.devs.kmhfldemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KmhflDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
